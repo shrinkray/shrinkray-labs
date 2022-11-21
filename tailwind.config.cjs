0e5c43727bc75876b3c8d80ffd305c54d453cd57
shrinkray <hello@gregmiller.io>
2022-11-21 18:37:46 -0500

    Install line-clamp plugin from @tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,8 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography'),
+						require('@tailwindcss/line-clamp')],
 	darkMode: 'class',
 };
 
